879143295852d4d25f6dd03eaeef341d63e09e0b
Florian Lorétan <florian.loretan@wunder.io>
2020-11-26 17:28:57 +0100

    Use a single list of entries to handle state.

--- src/NoteDispatcher.h
+++ src/NoteDispatcher.h
@@ -4,12 +4,62 @@ using namespace std;
 
 class NoteDispatcher {
   using NoteCallback = void (*)(int voice, int note, int velocity);
+
+  struct NoteEntry {
+    int note;
+    int velocity;
+    bool isPressed;
+    bool isPlaying;
+    int voiceIndex;
+  };
+
 private:
-  list<int> pressedNotes;
+  int voiceCount = 8;
+  list<NoteDispatcher::NoteEntry> entries;
+
   NoteCallback noteOnCallback;
   NoteCallback noteOffCallback;
+
+  bool isNotePressed(int note) {
+    for (NoteDispatcher::NoteEntry& entry : this->entries) {
+      if (entry.note == note && entry.isPressed) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  int getNextVoice(int note) {
+    // Find the next available voice index that isn't playing.
+    for (int voiceIndex = 0; voiceIndex < this->voiceCount; voiceIndex++) {
+      bool isUsed = false;
+      for (auto &entry : this->entries) {
+        if (entry.voiceIndex == voiceIndex && entry.isPlaying) {
+          // Already used, try the next voice.
+          isUsed = true;
+          break;
+        }
+      }
+
+      if (!isUsed) {
+        return voiceIndex;
+      }
+    }
+
+    // Nothing available, use the voice of the oldest used entry.
+    auto entry = this->entries.begin();
+    entry->isPlaying = false;
+    this->noteOffCallback(entry->voiceIndex, entry->note, 0);
+    return entry->voiceIndex;
+  }
+
 public:
   NoteDispatcher(){};
+  NoteDispatcher(int voiceCount)
+  {
+    this->voiceCount = voiceCount;
+  };
   ~NoteDispatcher(){};
 
   void setNoteOnCallback(NoteCallback callback) {
@@ -19,26 +69,41 @@ public:
     this->noteOffCallback = callback;
   };
 
+  void setMode(NoteDispatcher::Mode m) {
+    this->mode = m;
+  };
+
   void pressNote(int note, int velocity);
   void releaseNote(int note);
 };
 
 void NoteDispatcher::pressNote(int note, int velocity) {
-  this->pressedNotes.push_back(note);
+  if (this->isNotePressed(note)) {
+    return;
+  }
+
+  NoteDispatcher::NoteEntry entry = NoteDispatcher::NoteEntry();
+  entry.note = note;
+  entry.velocity = velocity;
+  entry.voiceIndex = this->getNextVoice(note);
+  entry.isPressed = true;
+  entry.isPlaying = true;
+
+  this->entries.push_back(entry);
 
   // Simply play the first voice.
-  this->noteOnCallback(0, note, velocity);
+  this->noteOnCallback(entry.voiceIndex, note, velocity);
 }
 
 void NoteDispatcher::releaseNote(int note) {
-  for (list<int>::iterator it=this->pressedNotes.begin(); it != this->pressedNotes.end(); it++) {
-    int pressedNote = *it;
-
-    if (pressedNote == note) {
-      this->pressedNotes.erase(it);
-
-      this->noteOffCallback(0, note, 0);
-      break;
+  for (auto it = this->entries.begin(); it != this->entries.end(); ) {
+    auto entry = *it;
+    if (entry.note == note) {
+      this->noteOffCallback(entry.voiceIndex, entry.note, 0);
+      this->entries.erase(it++);
+    }
+    else {
+      ++it;
     }
   }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,4 @@
 #include <unity.h>
-
 #include "../src/NoteDispatcher.h"
 
 /**
@@ -11,10 +10,10 @@ int playingNotes[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
  * Test callbacks.
  */
 void myCustomNoteOnCallback(int voice, int note, int velocity) {
-  playingNotes[note] = velocity;
+  playingNotes[voice] = velocity;
 }
 void myCustomNoteOffCallback(int voice, int note, int velocity) {
-  playingNotes[note] = 0;
+  playingNotes[voice] = 0;
 }
 
 void test_note_dispatcher(void) {
@@ -24,36 +23,66 @@ void test_note_dispatcher(void) {
   nd.setNoteOffCallback(myCustomNoteOffCallback);
 
   nd.pressNote(1, 123);
-  TEST_ASSERT_EQUAL(playingNotes[1], 123);
+  TEST_ASSERT_EQUAL(playingNotes[0], 123);
 
   nd.releaseNote(1);
-  TEST_ASSERT_EQUAL(playingNotes[1], 0);
+  TEST_ASSERT_EQUAL(playingNotes[0], 0);
 }
 
 void test_note_dispatcher_poly(void) {
+  NoteDispatcher nd = NoteDispatcher(2);
+
+  nd.setNoteOnCallback(myCustomNoteOnCallback);
+  nd.setNoteOffCallback(myCustomNoteOffCallback);
+
+  nd.pressNote(12, 123);
+  TEST_ASSERT_EQUAL(playingNotes[0], 123);
+  TEST_ASSERT_EQUAL(playingNotes[1], 0);
+  nd.pressNote(23, 124);
+  TEST_ASSERT_EQUAL(playingNotes[0], 123);
+  TEST_ASSERT_EQUAL(playingNotes[1], 124);
+  nd.releaseNote(12);
+  TEST_ASSERT_EQUAL(playingNotes[0], 0);
+  TEST_ASSERT_EQUAL(playingNotes[1], 124);
+  nd.releaseNote(23);
+  TEST_ASSERT_EQUAL(playingNotes[0], 0);
+  TEST_ASSERT_EQUAL(playingNotes[1], 0);
+
+  // Press more notes than voices available.
+  nd.pressNote(12, 102);
+  nd.pressNote(13, 103);
+  nd.pressNote(14, 104);
+  TEST_ASSERT_EQUAL(playingNotes[0], 104);
+  TEST_ASSERT_EQUAL(playingNotes[1], 103);
+
+  nd.releaseNote(12);
+  nd.releaseNote(13);
+  nd.releaseNote(14);
+}
+
+void test_note_dispatcher_repeated_note(void) {
   NoteDispatcher nd = NoteDispatcher();
 
   nd.setNoteOnCallback(myCustomNoteOnCallback);
   nd.setNoteOffCallback(myCustomNoteOffCallback);
 
   nd.pressNote(1, 123);
-  TEST_ASSERT_EQUAL(playingNotes[1], 123);
-  TEST_ASSERT_EQUAL(playingNotes[2], 0);
-  nd.pressNote(2, 124);
-  TEST_ASSERT_EQUAL(playingNotes[1], 123);
-  TEST_ASSERT_EQUAL(playingNotes[2], 124);
+  TEST_ASSERT_EQUAL(playingNotes[0], 123);
+
+  nd.pressNote(1, 124);
+  // Pressing an already pressed note does nothing.
+  TEST_ASSERT_EQUAL(playingNotes[0], 123);
+  TEST_ASSERT_EQUAL(playingNotes[1], 0);
   nd.releaseNote(1);
+  TEST_ASSERT_EQUAL(playingNotes[0], 0);
   TEST_ASSERT_EQUAL(playingNotes[1], 0);
-  TEST_ASSERT_EQUAL(playingNotes[2], 124);
-  nd.releaseNote(2);
-  TEST_ASSERT_EQUAL(playingNotes[1], 0);
-  TEST_ASSERT_EQUAL(playingNotes[2], 0);
 }
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_note_dispatcher);
     RUN_TEST(test_note_dispatcher_poly);
+    RUN_TEST(test_note_dispatcher_repeated_note);
     UNITY_END();
 
     return 0;
