cf83ac1b6f919a53a9dd7d2c4afde8538de2ab63
Florian Lorétan <florian.loretan@wunder.io>
2020-12-03 10:16:27 +0100

    Define a class for a synth voice.

---
+++ src/Voice.h
@@ -0,0 +1,50 @@
+#ifndef voice_H
+#define voice_H
+
+#include <Audio.h>
+#include <vector>
+
+using namespace std;
+
+class Voice {
+private:
+  vector<AudioConnection*> patchCords;
+public:
+  // These objects are public, so we can access them directly from the synth object.
+  AudioSynthWaveformModulated osc1;
+  AudioSynthWaveformModulated osc2;
+  AudioMixer4 mixer;
+  AudioEffectEnvelope env;
+
+  Voice(AudioMixer4 modulationInput) {
+    // Connect the frequency modulation input to the oscillators.
+    this->patchCords.push_back(new AudioConnection(modulationInput, 0, this->osc1, 0));
+    this->patchCords.push_back(new AudioConnection(modulationInput, 0, this->osc1, 1));
+    this->patchCords.push_back(new AudioConnection(modulationInput, 0, this->osc2, 0));
+    this->patchCords.push_back(new AudioConnection(modulationInput, 0, this->osc2, 1));
+
+    // Start oscillators.
+    this->osc1.begin(1.0, 880, WAVEFORM_SINE);
+    this->osc2.begin(1.0, 100, WAVEFORM_SAWTOOTH);
+
+    // Connect oscillators to voice mixer.
+    this->patchCords.push_back(new AudioConnection(this->osc1, 0, this->mixer, 0));
+    this->patchCords.push_back(new AudioConnection(this->osc2, 0, this->mixer, 1));
+
+    // Setup mixer.
+    this->mixer.gain(0, 1.0);
+    this->mixer.gain(1, 1.0);
+
+    // Connect mixer to envelope.
+    this->patchCords.push_back(new AudioConnection(this->mixer, 0, this->env, 0));
+    this->env.attack(20);
+    this->env.decay(100);
+    this->env.sustain(0.7);
+    this->env.release(300);
+  };
+  ~Voice() {
+
+  };
+};
+
+#endif
